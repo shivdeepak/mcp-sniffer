812f8e3e665048a6042971d0d906c7240af75a4c
Shiv Deepak <shivdeepak@gmail.com>
2025-04-27 16:03:17 -0700

    eslint and prettier setup

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,28 +1,39 @@
 import js from '@eslint/js'
-import globals from 'globals'
+import json from '@eslint/json'
+import eslintConfigPrettier from 'eslint-config-prettier/flat'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import { defineConfig } from 'eslint/config'
+import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default defineConfig([
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
+    files: ['src/**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    plugins: { js },
+    extends: ['js/recommended'],
   },
-)
+  {
+    files: ['src/**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    languageOptions: { globals: globals.node },
+  },
+  tseslint.configs.recommended,
+  {
+    files: ['**/*.json'],
+    plugins: { json },
+    language: 'json/jsonc',
+    extends: ['json/recommended'],
+  },
+  eslintConfigPrettier,
+  eslintPluginPrettierRecommended,
+  reactHooks.configs['recommended-latest'],
+  reactRefresh.configs.recommended,
+  {
+    ignores: [
+      '../mcp_sniffer/frontend/**',
+      'node_modules/**',
+      'package-lock.json',
+    ],
+  },
+])
